c97e4a59c04316c241acb42c1bcf6cb982a921da
beepscore <support@beepscore.com>
2010-02-28 20:26:37 -0800

    Delete commented out code.

--- Classes/TrendyViewController.m
+++ Classes/TrendyViewController.m
@@ -13,37 +13,16 @@
 
 @synthesize webView, activityIndicator, downloadedData, baseURL, connection;
 
-/*
- // The designated initializer. Override to perform setup that is required before the view is loaded.
- - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
- if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
- // Custom initialization
- }
- return self;
- }
- */
-
-/*
- // Implement loadView to create a view hierarchy programmatically, without using a nib.
- - (void)loadView {
- }
- */
-
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
-    [super viewDidLoad];   
+    [super viewDidLoad];
+    [self readTrendsFile:self];
 }
 
 
-/*
- // Override to allow orientations other than the default portrait orientation.
- - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
- // Return YES for supported orientations
- return (interfaceOrientation == UIInterfaceOrientationPortrait);
- }
- */
-
+#pragma mark -
+#pragma mark memory management
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
@@ -51,19 +30,21 @@
 	// Release any cached data, images, etc that aren't in use.
 }
 
+
 - (void)viewDidUnload {
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
 }
 
-- (void)setView:(UIView *)newView
-{
+
+- (void)setView:(UIView *)newView {
     if (nil == newView) {
         self.webView = nil;
         self.activityIndicator = nil;
     }    
     [super setView:newView];
 }
+
 
 - (void)dealloc {
     [webView release], webView = nil;
@@ -75,6 +56,7 @@
     [super dealloc];
 }
 
+
 #pragma mark -
 #pragma mark handle UI inputs
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
@@ -84,14 +66,15 @@
             || (interfaceOrientation == UIInterfaceOrientationLandscapeRight));
 }
 
+
 #pragma mark IBAction
-- (IBAction)readTrendsFile:(id)sender
-{
+- (IBAction)readTrendsFile:(id)sender {
+    // NSURL *url = [[NSURL alloc] initWithString:@"http://google.com"];
+    
     NSString *trendsPath =[[NSBundle mainBundle]
                            pathForResource:@"trends" ofType:@"html"];
     NSURL *url = [[NSURL alloc] initFileURLWithPath:trendsPath isDirectory:NO];
     
-    //NSURL *url = [[NSURL alloc] initWithString:@"http://google.com"];
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
     self.baseURL = url;
     [url release], url = nil;
@@ -101,7 +84,10 @@
     self.downloadedData = data;
     [data release], data = nil;
     
-    NSURLConnection *newConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:YES];
+    NSURLConnection *newConnection = [[NSURLConnection alloc] 
+                                      initWithRequest:request 
+                                      delegate:self 
+                                      startImmediately:YES];
     if (nil == newConnection) {
         NSLog(@"Could not create connection");
     } else {
@@ -110,25 +96,23 @@
     }
     
     [newConnection release], newConnection = nil;
-    [request release], request = nil;
-    
+    [request release], request = nil;    
 }
 
-#pragma mark NSURLConnection delegate
 
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
-{
+#pragma mark NSURLConnection delegate
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
     [self.downloadedData setLength:0];
     
 }
 
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
-{
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
     [self.downloadedData appendData:data];
 }
 
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
-{
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
     NSLog(@"Connection failed: %@", error);
     [self.activityIndicator stopAnimating];
     // We no longer need the connection, data, or baseURL.
@@ -137,8 +121,8 @@
     self.baseURL = nil;
 }
 
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection
-{
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
     [self.webView loadData:self.downloadedData MIMEType:nil textEncodingName:nil baseURL:self.baseURL];
     [self.activityIndicator stopAnimating];
     // We no longer need the connection, data, or baseURL.
